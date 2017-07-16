f38eb3f07c1c421e87f487940446c420d7ef9fd8
max <max@max>
2017-07-16 14:48:52 +0300

    test

--- src/logic/logic.cpp
+++ src/logic/logic.cpp
@@ -1,1 +1,16 @@
+#include "Arduino.h"
 
+int ledPin = 13;                // LED подсоединен к выводу 13
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);      // устанавливаем вывод 13 как выход
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);   // включаем LED
+  delay(1000);                  // пауза 1 секунда (1000 мс)
+  digitalWrite(ledPin, LOW);    // выключаем LED
+  delay(1000);                  // пауза 1 секунда (1000 мс)
+}
